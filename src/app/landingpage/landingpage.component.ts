ec2830100682b3d3b6aa234d1307b3ab775b90c2
Nick Summa <nicholas.summa@gmail.com>
2019-02-21 19:51:35 -0600

    ngIf/else practice

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -8,12 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class LandingpageComponent implements OnInit {
     searchItem = '';
     searchButton = false;
-    searchInput = false;
+    searchInput = true;
 
   constructor() {
       setTimeout(() => {
         this.searchButton = true; /* after 2 seconds the search button will be enabled*/
-        this.searchInput = true;  /* after 2 seconds the input value will show*/
       }, 2000);
     }
 
@@ -24,4 +23,7 @@ export class LandingpageComponent implements OnInit {
       this.searchItem = (<HTMLInputElement>event.target).value;  /* binding to the what the user is inputing in the search bar*/
     }
 
+  onSearchFocus () {    /* will change the DOM when the search input field is focused */
+    this.searchInput = false;
+  }
 }
