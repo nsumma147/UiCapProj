97f0469077fa6fbd2739a8c8bb6ae81db098067d
Nick Summa <nicholas.summa@gmail.com>
2019-02-24 14:29:43 -0600

    reverting to earlier code due to bug

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -8,11 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class LandingpageComponent implements OnInit {
     searchItem = '';
     searchButton = false;
-    searchInput = true;
+    searchInput = false;
 
   constructor() {
       setTimeout(() => {
         this.searchButton = true; /* after 2 seconds the search button will be enabled*/
+        this.searchInput = true;  /* after 2 seconds the input value will show*/
       }, 2000);
     }
 
@@ -22,8 +23,4 @@ export class LandingpageComponent implements OnInit {
   onSearch(event: Event) {
       this.searchItem = (<HTMLInputElement>event.target).value;  /* binding to the what the user is inputing in the search bar*/
     }
-
-  onSearchFocus () {    /* will change the DOM when the search input field is focused */
-    this.searchInput = false;
-  }
 }
